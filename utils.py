6c96811c1cfed34ade21c1bb3524b47afb37c26e
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2022-04-09 11:09:15 +0300

    updated utilities

--- utils.py
+++ utils.py
@@ -2,14 +2,14 @@
 import streamlit as st
 import requests
 
-def main_heading_text():
+def main_heading_text_cat():
     st.markdown("""
 
         <style>
         .big-font {
             text-align: center;
             padding-top: 0.5px;
-            font-size: calc(0.70em + 4.5vmin);
+            font-size: calc(0.50em + 4.5vmin);
             font-family: sans-serif;
             font-weight: bold;
         }
@@ -17,6 +17,22 @@ def main_heading_text():
         """, unsafe_allow_html = True)
 
     st.markdown('<p class="big-font">WAFIAPPS CATEGORY VIEWER</p>', unsafe_allow_html = True)
+
+def main_heading_text_vendor():
+    st.markdown("""
+
+        <style>
+        .big-font {
+            text-align: center;
+            padding-top: 0.5px;
+            font-size: calc(0.50em + 4.5vmin);
+            font-family: sans-serif;
+            font-weight: bold;
+        }
+        </style>
+        """, unsafe_allow_html = True)
+
+    st.markdown('<p class="big-font">WAFIAPPS VENDOR PORTAL</p>', unsafe_allow_html = True)
 
 #if False:
 
@@ -32,3 +48,16 @@ def intro_lottie(url: str):
         return None
 
     return r.json()
+
+def break_loop_vendor_viewer():
+    i = 0
+    while i < 5:
+        st.markdown('<p></p>', unsafe_allow_html = True)
+        i += 1
+
+def hide_anchor_link():
+    st.markdown("""
+        <style>
+        .css-15zrgzn {display: none}
+        </style>
+        """, unsafe_allow_html=True)
